a9a5d83967a707315e8ed43228ac7b41c94f5b2a
Archie <archibaldofgenoa@yandex.ru>
2024-11-10 01:00:11 +0300

    prettier-tailwind installed

--- app/Input.tsx
+++ app/Input.tsx
@@ -1,7 +1,10 @@
 export function Input() {
   return (
     <>
-      <input className="xl: w-[790px] h-12 bg-input-grey placeholder:text-vanilla " placeholder="Enter the city for the weather forecast of click to auto-detect"></input>
+      <input
+        className=" bg-input-grey placeholder:text-vanilla focus:bg-vanilla focus:placeholder:text-dark-grey outline-none w-[790px] h-12"
+        placeholder="Enter the city for the weather forecast of click to auto-detect"
+      ></input>
     </>
   );
 }
--- app/ui/colors.ts
+++ app/ui/colors.ts
@@ -1,4 +1,5 @@
 export const colors = {
   "input-grey": "#bcc8c4",
+  "dark-grey": "#6f7f8a",
   vanilla: "#f5f5f5",
 };
